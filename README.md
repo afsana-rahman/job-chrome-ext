8f643243f279888b59bf86b983be8c3042de6301
afsana-rahman <54005308+afsana-rahman@users.noreply.github.com>
2023-07-25 00:21:12 +0000

    Finished "Edit" and "Save" Functions

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # job-chrome-ext
 A Google Chrome extension that stores resume info. Makes the job application process a little easier.
+
+Showcases knowledge in HTML/CSS/JS stack.
--- script.js
+++ script.js
@@ -19,8 +19,25 @@ function cancelJob() {
     document.getElementById("jdescrip").value = "";
 }
 
+function createEDButtons(buttonCell, numRow) {
+    var editButton = document.createElement("button");
+    editButton.innerText = "Edit";
+    editButton.id = "edit" + numRow;
+    editButton.addEventListener("click", () => {
+        editJob(editButton.id);
+    });
+    var deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.id = "delete" + numRow;
+    deleteButton.addEventListener("click", () => {
+        deleteJob(deleteButton.id);
+    });
+    buttonCell.appendChild(editButton);
+    buttonCell.appendChild(deleteButton);
+}
+
 function addJob() {
-    const details = [];
+    var details = [];
     details[0] = document.getElementById("jtitle").value;
     details[1] = document.getElementById("jemployer").value;
     details[2] = document.getElementById("jstart").value + " - " + document.getElementById("jend").value;
@@ -28,50 +45,103 @@ function addJob() {
     details[4] = document.getElementById("jdescrip").value;
 
     const table = document.getElementById("work-exp-table");
-    var numRow = table.rows.length-1;
+    const numRow = table.rows.length;
     var row = table.insertRow(numRow);
     for(let i = 0; i <= 4; i++) {
         var cell = row.insertCell(i);
         cell.innerHTML = details[i];
     }
-    
-    var cell6 = row.insertCell(5);
-    var editButton = document.createElement("button");
-    editButton.innerText = "Edit";
-    editButton.id = "edit" + numRow;
-    editButton.addEventListener("click", () => {
-        editJob();
-    });
-    var saveButton = document.createElement("button");
-    saveButton.innerText = "Save";
-    saveButton.id = "save" + numRow;
-    saveButton.style.display = "none";
-    editButton.addEventListener("click", () => {
-        saveJob();
-    });
-    var deleteButton = document.createElement("button");
-    deleteButton.innerText = "Delete";
-    deleteButton.id = "delete" + numRow;
-    editButton.addEventListener("click", () => {
-        deleteJob();
-    });
-    cell6.appendChild(editButton);
-    cell6.appendChild(saveButton);
-    cell6.appendChild(deleteButton);
-
+    var buttonCell = row.insertCell(5);
+    createEDButtons(buttonCell, numRow);
     cancelJob();
 }
 
-function editJob() {
-    var editButton = document.getElementById("edit");
-    editButton.style.display = "none";
-    var saveButton = document.getElementById("save");
-    saveButton.style.display = "inline-block";
+function editJob(clickid) {
+    const numRow = clickid.substring(4);
+    const num = Number(numRow);
+    const table = document.getElementById("work-exp-table");
+    var row = table.rows[num].cells;
+    
+    var prev = [];
+    for(let i = 0; i <= 4; i++) {
+        prev[i] = row[i].innerHTML;
+    }
+    table.deleteRow(num);
+    
+    var newRow = table.insertRow(num);
+    for(let i = 0; i <= 4; i++) {
+        var cell = newRow.insertCell(i);
+        var text = document.createElement("input");
+        text.id = num + "text" + i;
+        text.type = "text";
+        text.value = prev[i];
+        cell.appendChild(text);
+    }
+
+    var buttonCell = newRow.insertCell(5);
+    var saveButton = document.createElement("button");
+    saveButton.innerText = "Save";
+    saveButton.id = "save" + num;
+    saveButton.addEventListener("click", () => {
+        saveJob(saveButton.id);
+    });
+    buttonCell.appendChild(saveButton);
+    var cancelButton = document.createElement("button");
+    cancelButton.innerText = "Cancel";
+    cancelButton.id = "cancel" + num;
+    cancelButton.addEventListener("click", () => {
+        cancelEdit(cancelButton.id, prev);
+    });
+    buttonCell.appendChild(cancelButton);
 }
 
-function saveJob() {
-    var saveButton = document.getElementById("save");
-    saveButton.style.display = "none";
-    var editButton = document.getElementById("edit");
-    editButton.style.display = "inline-block";
+function saveJob(clickid) {
+    const numRow = clickid.substring(4);
+    const num = Number(numRow);
+    const table = document.getElementById("work-exp-table");
+    var row = table.rows[num].cells;
+    
+    var prev = [];
+    for(let i = 0; i <= 4; i++) {
+        var cellId = num + "text" + i;
+        var cell = document.getElementById(cellId);
+        prev[i] = cell.value;
+    }
+    table.deleteRow(num);
+
+    var newRow = table.insertRow(num);
+    for(let i = 0; i <= 4; i++) {
+        var cell = newRow.insertCell(i);
+        cell.innerHTML = prev[i];
+    }
+    var buttonCell = newRow.insertCell(5);
+    createEDButtons(buttonCell, num);
+}
+
+function cancelEdit(clickid, prev) {
+    const numRow = clickid.substring(6);
+    const num = Number(numRow);
+    const table = document.getElementById("work-exp-table");
+    table.deleteRow(num)
+
+    var row = table.insertRow(num);
+    for(let i = 0; i <= 4; i++) {
+        var cell = row.insertCell(i);
+        cell.innerHTML = prev[i];
+    }
+    var buttonCell = row.insertCell(5);
+    createEDButtons(buttonCell, num);
+}
+
+function deleteJob(clickid) {
+    if(window.confirm("Are you sure you want to delete this job?")) {
+        const numRow = Number(clickid.substring(6));
+        document.getElementById("work-exp-table").deleteRow(numRow);
+
+        for(let i = 1; i < document.getElementById("work-exp-table").rows.length; i++) {
+            const row = document.getElementById("work-exp-table").rows[i].cells;
+            row[5].firstChild.id = "edit" + i;
+            row[5].lastChild.id = "delete" + i;
+        }
+    }
 }
